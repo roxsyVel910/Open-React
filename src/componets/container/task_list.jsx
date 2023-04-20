58bafbbf237afb0b41c81d7d81be620e51023984
Roxsy <roxsy.dj@gmail.com>
2023-04-20 10:18:25 -0500

    borrando contenido de task list

--- src/componets/container/task_list.jsx
+++ src/componets/container/task_list.jsx
@@ -1,16 +1,1 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
-export default class task_list extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
-
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
