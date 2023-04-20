94758bdcffd537f91895aeea25fe33af88dcad8f
Roxsy <roxsy.dj@gmail.com>
2023-04-20 10:22:49 -0500

    componente task_list

--- src/componets/container/task_list.jsx
+++ src/componets/container/task_list.jsx
@@ -1,1 +1,30 @@
 
+import React from 'react'
+import { Task } from '../../models/task.class'
+import { LEVELS } from '../../models/levels.enum'
+import TaskComponent from '../pure/task';
+
+const TaskListComponent = (props) => {
+    const defaultTask = new Task ('Martin', 'lorem input', true, LEVELS.NORMAL);
+
+    const changeState = (id) => {
+        console.log('TODO: Csmbiar estado de una tarea')
+    }
+
+
+  return (
+    <div>
+    <div>
+        Your Tasks:
+    </div>
+    {/* Todo aplicar un form/map para renderixar un a lista*/}
+    <TaskComponent task = { defaultTask}></TaskComponent>
+      
+    </div>
+  )
+}
+
+
+
+export default TaskListComponent;
+
